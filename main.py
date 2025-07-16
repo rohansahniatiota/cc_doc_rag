16524704040610a607006f7e75b4e2bee866306c
Rohan Sahni <rohan.sahni@iotaanalytics.com>
2025-07-16 14:11:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,12 +39,84 @@ async def get_pdf_documents():
         ),
         Document(
             fileName="Machine Learning",
-            url="https://github.com/tanishachandani/AWS-Notes/blob/main/15.%20Machine%20Learning.pdf"
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/1.%20Basics%20and%20overview.pdf"
         ),
         Document(
             fileName="Deployment",
             url="https://github.com/tanishachandani/AWS-Notes/blob/main/9.%20Deployment%20%26%20Managing%20Infrastructure%20at%20Scale.pdf"
         ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/11.%20Cloud%20Integrations.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/12.%20Cloud%20Monitoring.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/13.%20VPC%20%26%20Networking.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/14.%20Security%20%26%20Compliance.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/16.%20Account%20Management%2C%20Billing%20%26%20Support.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/17.%20Advanced%20Identity.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/18.%20Other%20Services.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/19.%20AWS%20Architecting%20%26%20Ecosystem.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/2.%20IAM%20-%20Identity%20Access%20Management.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/20.%20Short%20Notes.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/3.%20EC2%20-%20Elastic%20Cloud%20Compute.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/4.%20EC2%20Storage%20and%20stuff.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/5.%20ELB%20%26%20ASG.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/7.%20Databases%20%26%20Analytics.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://github.com/tanishachandani/AWS-Notes/blob/main/8.%20Other%20Compute%20Services.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://drive.google.com/file/d/0B3vyNXp6qDWwTWdzLTJjOUtTcGc/edit?resourcekey=0-f1hRee21RT_lRLiIiequNw"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://www.emgywomenscollege.ac.in/templateEditor/kcfinder/upload/files/Encyclopaedia%20in%20Mathematics.pdf"
+        ),
+        Document(
+            fileName="Deployment",
+            url="https://darwin-online.org.uk/converted/pdf/1823_Encyclopedia_A770.06.pdf"
+        )
         
         
     ]
